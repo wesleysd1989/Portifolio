c257c0453214a0d64c7f02c00acbbeeef61a8ca9
wesley dos santos dias <wesleysd1989@gmail.com>
2019-04-08 17:19:45 -0300

    add view NavBar

---
+++ frontend/src/views/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+    <a className="navbar-brand mb-0 h1" href="#/">
+      Wesley Dias
+    </a>    
+  </nav>
+)
+export default NavBar
